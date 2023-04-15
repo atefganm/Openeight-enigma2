b7ad14dc8b0c8789616cb8ec886a4a2ab294fc91
atefganm <atef_anim_1974@yahoo.com>
2023-04-16 01:13:23 +0300

    sigc: update to version 3 Thanks to @Athanasios Oikonomou

--- lib/gdi/grc.h
+++ lib/gdi/grc.h
@@ -190,7 +190,7 @@ public:
 	void lock();
 	void unlock();
 
-	sigc::signal0<void> notify;
+	sigc::signal<void()> notify;
 
 	void setSpinnerDC(gDC *dc) { m_spinner_dc = dc; }
 	void setSpinnerOnOff(int onoff) { m_spinneronoff = onoff; }
